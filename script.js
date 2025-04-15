1fa5076ec25925a991717bdcbe53ab13de1c6fbb
ErickBernat <erickduartebr@gmail.com>
2025-04-15 11:33:26 -0300

    Add funcao diminuir tela

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ let botoes =  document.querySelectorAll('.botoes');
 let arrayCobra = [];
 let gameover = false;
 let macaX=0, macaY=0;
-
+let bombaX=0, bombaY=0;
+let width = 50, height= 80;
 let setintervalId =0;
+let setintervalId2 =0;
 let cobraX=5,cobraY=5;
 let velocidadeX=0, velocidadeY=0;
-
+let comeuMaca = false;
+let contador=1
+let grid=30;
 
 document.addEventListener('DOMContentLoaded' ,()=>{
+    containerJogo.style.width = `${width}%`;
+    containerJogo.style.height = `${height}%`;
 
     menuGame.innerHTML+=`
     <div class="tituloGame">
@@ -27,25 +33,39 @@ document.addEventListener('DOMContentLoaded' ,()=>{
                         <button class="botoes">Menu</button>
                     </div>
     `
-
 })
 
 
 geraMaca()
-adicionaMuros()
+
 function iniciaJogo(){
 let html;
-
 
 if(gameover){
     reiniciaJogo()
     return
 }
+
+
+
+
+
     html =`
         <div id='maca' class='maca' style="grid-area:${macaY}/${macaX};" ></div>
     `   
+    html +=`
+    <div id='bomba' class='bomba' style="grid-area:${bombaY}/${bombaX};" ></div>
+` 
+      html+=`<div id='muro' class='muro' style=" grid-area:${1}/${7};" ></div>`
 
     if(cobraX === macaX && cobraY === macaY){
+            console.log('Comeu')
+            width+=1
+            height+=1
+            grid+=3
+            containerJogo.style.width = `${width}%`;
+            containerJogo.style.height = `${height}%`;
+
         geraMaca();
  
         let ultimaParte = arrayCobra[arrayCobra.length - 1];
@@ -67,14 +87,15 @@ if(gameover){
             gameover = true
         }
     }
+    console.log(arrayCobra[0][0])
 
-    if((arrayCobra[0][0] < 0 || arrayCobra[0][0] >30 || arrayCobra[0][1] < 0 || arrayCobra[0][1] >30  )){
+    if((arrayCobra[0][0] <= 0 || arrayCobra[0][0] >= 30 || arrayCobra[0][1] <= 0 || arrayCobra[0][1] >=30  )){
         gameover = true;
     }
 
 
     html+=`<div id='cobra' class='cobraCabeca' style="grid-area:${arrayCobra[0][1]}/${arrayCobra[0][0]};" ></div>`
-
+   
 for(let contador =1; contador<arrayCobra.length;contador++){
     
     html+=`<div id='cobra' class='cobra' style="grid-area:${arrayCobra[contador][1]}/${arrayCobra[contador][0]};" ></div>`
@@ -89,15 +110,7 @@ function adidionaEventoBotoes(){
     menuGame.style.display = 'none'
 }
 
-function adicionaMuros(){
-    containerJogo.innerHTML+=`
-        <div id='muro${1}' class='muro' style="grid-area:${10}/${10}};"></div>
-        `
-    for (let cont =0 ;cont <=30 ;cont++){
-        console.log('oi')
-        
-    }
-}
+
 
 document.addEventListener('keydown',movimentaCobra)
 
@@ -130,8 +143,29 @@ function reiniciaJogo(){
 }
 
 function geraMaca(){
-    macaX = Math.floor(Math.random(30)*30)+1;
-    macaY =  Math.floor(Math.random(30)*30)+1;
+    macaX = Math.floor(Math.random(grid)*grid)+1;
+    macaY =  Math.floor(Math.random(grid)*grid)+1;
+}
+function geraBomba(){
+    bombaX = Math.floor(Math.random(grid)*grid)+1;
+    bombaY =  Math.floor(Math.random(grid)*grid)+1;
 }
 
-setintervalId = setInterval(iniciaJogo,100)
+
+function verificaComeuMaca(){
+    console.log('entrou')
+    console.log(comeuMaca)
+        console.log('naoComeu')
+        width-=contador
+        height-=contador
+        grid-=3;
+        containerJogo.style.width = `${width}%`;
+        containerJogo.style.height = `${height}%`;
+
+    
+}
+
+setintervalId = setInterval(geraBomba,3000);
+
+setintervalId = setInterval(iniciaJogo,200);
+setintervalId = setInterval(verificaComeuMaca,3000);
